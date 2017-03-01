877ee4f0591728e51ef86f0298b50f96e6823b95
Uskinn <s.nevzor@yahoo.com>
2017-03-01 10:35:47 -0500

    saving progress

---
+++ 12String-Find-Longest-Prefix.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func findLongestPrefixIn(thisString: String) -> String {
+    
+    
+    
+    
+    return ""
+}
+
+let firstString = "swift switch swill swim"
+let secondString = "flip flap flop"
+
+print(findLongestPrefixIn(thisString: firstString))
