0f419688eae133cdfded5a2b3bfe89911bcc6067
Uskinn <s.nevzor@yahoo.com>
2017-03-02 16:12:28 -0500

    solved

--- 12String-Find-Longest-Prefix.playground/Contents.swift
+++ 12String-Find-Longest-Prefix.playground/Contents.swift
@@ -5,11 +5,25 @@ import UIKit
 var str = "Hello, playground"
 
 func findLongestPrefixIn(thisString: String) -> String {
+    var prefixString = ""
+    var currentPrefix = ""
     
+    let stringParts = thisString.components(separatedBy: " ")
     
+    guard let first = stringParts.first else { return "" }
     
-    
-    return ""
+    for char in first.characters {
+        currentPrefix.append(char)
+        print(currentPrefix)
+        
+        for word in stringParts {
+            if !word.hasPrefix(currentPrefix) {
+                return prefixString
+            }
+        }
+        prefixString = currentPrefix
+    }
+    return prefixString
 }
 
 let firstString = "swift switch swill swim"
